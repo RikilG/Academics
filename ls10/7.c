f83fd54f8d3e41c08342a749baa6ddf55fcb800a
Rikil G <rikil.gajarla@gmail.com>
2019-04-16 07:18:31 +0530

    completion round 1

--- ls10/7.c
+++ ls10/7.c
@@ -2,10 +2,6 @@
 #include<stdlib.h>
 #include<string.h>
 
-int cmp(const void *a, const void *b) {
-    return *((int*)a)-*((int*)b);
-}
-
 int main() {
     int n=0,arr[1000],count=0;
     char ch;
